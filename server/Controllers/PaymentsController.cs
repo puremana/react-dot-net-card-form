bc3411312f02f1e56325b8d1700c89c83510e19e
puremana <puremana@gmail.com>
2022-03-11 16:31:52 +1300

    Adding validation

--- server/Controllers/PaymentsController.cs
+++ server/Controllers/PaymentsController.cs
@@ -42,63 +42,87 @@ namespace server.Controllers
             return payment;
         }
 
-        // PUT: api/Payments/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutPayment(long id, Payment payment)
-        {
-            if (id != payment.Id)
-            {
-                return BadRequest();
-            }
+        // // PUT: api/Payments/5
+        // // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // [HttpPut("{id}")]
+        // public async Task<IActionResult> PutPayment(long id, Payment payment)
+        // {
+        //     if (id != payment.Id)
+        //     {
+        //         return BadRequest();
+        //     }
 
-            _context.Entry(payment).State = EntityState.Modified;
+        //     _context.Entry(payment).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PaymentExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //     try
+        //     {
+        //         await _context.SaveChangesAsync();
+        //     }
+        //     catch (DbUpdateConcurrencyException)
+        //     {
+        //         if (!PaymentExists(id))
+        //         {
+        //             return NotFound();
+        //         }
+        //         else
+        //         {
+        //             throw;
+        //         }
+        //     }
 
-            return NoContent();
-        }
+        //     return NoContent();
+        // }
 
         // POST: api/Payments
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<Payment>> PostPayment(Payment payment)
         {
+
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            // Custom Validation
+            // Name no greater than 50 characters
+            if (!(payment.Name.Length > 0 && payment.Name.Length <= 50)) {
+                return BadRequest("Invalid name.");
+            }
+
+            // Card is only digits && is between 8 and 19 in length
+            // if (!(payment.Card.All(char.IsDigit) && payment.Card.Length >= 8 && payment.Card.Length <= 19)) {
+            //     return BadRequest("Invalid Card Number.");
+            // }
+            // CVC is only digit and 3 characters in length
+            if (!(payment.CVC.All(char.IsDigit) && payment.CVC.Length == 3)) {
+                return BadRequest("Invalid CVC.");
+            }
+            // Check expiry is in future
+            DateTime today = DateTime.Now;
+            if (!(DateTime.Compare(payment.Expiry, today) > 0)) {
+                return BadRequest("Invalid expiry.");
+            }
+
             _context.Payments.Add(payment);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction(nameof(GetPayment), new { id = payment.Id }, payment);
         }
 
-        // DELETE: api/Payments/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePayment(long id)
-        {
-            var payment = await _context.Payments.FindAsync(id);
-            if (payment == null)
-            {
-                return NotFound();
-            }
+        // // DELETE: api/Payments/5
+        // [HttpDelete("{id}")]
+        // public async Task<IActionResult> DeletePayment(long id)
+        // {
+        //     var payment = await _context.Payments.FindAsync(id);
+        //     if (payment == null)
+        //     {
+        //         return NotFound();
+        //     }
 
-            _context.Payments.Remove(payment);
-            await _context.SaveChangesAsync();
+        //     _context.Payments.Remove(payment);
+        //     await _context.SaveChangesAsync();
 
-            return NoContent();
-        }
+        //     return NoContent();
+        // }
 
         private bool PaymentExists(long id)
         {
--- server/Models/Payment.cs
+++ server/Models/Payment.cs
@@ -1,11 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace server.Models
 {
     public class Payment
     {
         public long Id { get; set; }
+        
+        [Required]
+        [StringLength(50)]
         public string? Name { get; set; }
+
+        [Required]
+        [CreditCard]
         public string? Card { get; set; }
+
+        [Required]
+        [StringLength(3)]
         public string? CVC { get; set; }
+
+        [Required]
         public DateTime Expiry { get; set; }
     }
 }
